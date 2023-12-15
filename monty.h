3e7168a989536447b69f2c26bea863e4a5b53544
NatashaMuhanji <natashamuhanji@gmail.com>
2023-12-15 11:50:57 -0800

    Files

--- monty.h
+++ monty.h
@@ -52,21 +52,21 @@ void find_func(char *, char *, int, int);
 /*Stack operations*/
 stack_t *create_node(int n);
 void free_nodes(void);
-void print_stack(stack_t **, unsigned int);
-void add_to_stack(stack_t **, unsigned int);
+void print_to_stack(stack_t **, unsigned int);
+void add_on_stack(stack_t **, unsigned int);
 void add_to_queue(stack_t **, unsigned int);
 
 void call_fun(op_func, char *, char *, int, int);
 
-void print_top(stack_t **, unsigned int);
-void pop_top(stack_t **, unsigned int);
-void nop(stack_t **, unsigned int);
+void print_stack_top(stack_t **, unsigned int);
+void pop_stack_top(stack_t **, unsigned int);
+void nope(stack_t **, unsigned int);
 void swap_nodes(stack_t **, unsigned int);
 
 /*Math operations with nodes*/
-void add_nodes(stack_t **, unsigned int);
-void sub_nodes(stack_t **, unsigned int);
-void div_nodes(stack_t **, unsigned int);
+void adding_nodes(stack_t **, unsigned int);
+void subtract_nodes(stack_t **, unsigned int);
+void divide_nodes(stack_t **, unsigned int);
 void multiply_nodes(stack_t **, unsigned int);
 void modify_nodes(stack_t **, unsigned int);
 
